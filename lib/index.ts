67b7ca11c5314d9a542cb525fa69f41d2c9d3c65
Prakash.vblog@gmail.com <Prakash.vblog@gmail.com>
2023-03-25 10:37:55 +1100

    Add standard SQS queue

--- lib/index.ts
+++ lib/index.ts
@@ -1,21 +1,2 @@
-// import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export interface AppProps {
-  // Define construct properties here
-}
-
-export class App extends Construct {
-
-  constructor(scope: Construct, id: string, props: AppProps = {}) {
-    super(scope, id);
-
-    // Define construct contents here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
-}
+export * from './standard-sqs/sqs'
