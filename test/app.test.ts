c3818513b770d9719ec6f6cecae9961ee3f8135a
Prakash.vblog@gmail.com <Prakash.vblog@gmail.com>
2023-03-25 22:52:23 +1100

    Add a simple test

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,18 +1,38 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as App from '../lib/index';
+import * as cdk from 'aws-cdk-lib';
+import { Stack } from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as App from '../lib/index';
+import { StandardSQS, StandardSQSProps } from '../lib/index';
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/index.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//   const stack = new cdk.Stack(app, "TestStack");
-//   // WHEN
-//   new App.App(stack, 'MyTestConstruct');
-//   // THEN
-//   const template = Template.fromStack(stack);
+describe('StandardSQS', () => {
+    let stack: Stack;
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+    beforeEach(() => {
+        const app = new cdk.App();
+        stack = new Stack(app, "TestStack");
+    });
+
+    test('should create a standard SQS queue with a dead letter queue', () => {
+        // GIVEN
+        const props: StandardSQSProps = {
+            queueName: 'MyQueue'
+        };
+
+        // WHEN
+        new StandardSQS(stack, 'MyQueue', props);
+
+        const template = Template.fromStack(stack);
+
+        // Assert it creates the function with the correct properties...
+        template.hasResourceProperties("AWS::SQS::Queue", {
+            QueueName: "MyQueue-dlq"
+        });
+
+        template.hasResourceProperties("AWS::SQS::Queue", {
+            QueueName: "MyQueue"
+        });
+
+    });
 });
